27d5c1e50a079ad3c620d240058f2ebae2a09c32
Stan <6561374+Mala9898@users.noreply.github.com>
2021-02-26 18:00:33 +0100

    navbar skelton, before slate.js

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
-#2DT301-coding-platform
+# 2DT301-coding-platform
 
 ## Description
 Online frictionless IDE.
 
 ## Build Instructions
-todo...
+```
+  npm start
+    Starts the development server.
+
+  npm run build
+    Bundles the app into static files for production.
+
+  npm test
+    Starts the test runner.
+
+  npm run eject
+    Removes this tool and copies build dependencies, configuration files
+    and scripts into the app directory. If you do this, you can’t go back!
+```
 
 ## Run instructions
 todo...
---
+++ frontend/src/App.js
@@ -0,0 +1,92 @@
+import logo from './logo.svg';
+import './App.css';
+
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"
+import styled from 'styled-components'
+
+const StyledNavLink = styled(Link)`
+  color: #ffffff;
+  /* background: #256ce1; */
+  border-radius: 4px;
+  text-decoration: none;
+  /* padding: 1rem 2rem; */
+  margin: 1rem 1rem;
+  outline: none;
+  /* :hover {
+    color: #b700ff;
+  } */
+  transition: all 0.2s ease-in-out;
+  :hover {
+    transition: all 0.2s ease-in-out;
+    /* background: #fff; */
+    background: #0c78d1;
+    /* color: #010606; */
+    color: #ffffff;
+  }
+`
+const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  height: 40px;
+  
+`
+const UlList = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  background-color: #0d92ff;
+`
+
+
+const App = () => {
+  return (
+    <Router>
+      <nav>
+          <UlList>
+            <NavMenu>
+              <li>
+                <StyledNavLink to="/">Coding Platform</StyledNavLink>
+              </li>
+            </NavMenu>
+            <NavMenu>
+              <li>
+                <StyledNavLink to="/">Home</StyledNavLink>
+              </li>
+              <li>
+                <StyledNavLink to="/about">About</StyledNavLink>
+              </li>
+              <li>
+                <StyledNavLink to="/users">Users</StyledNavLink>
+              </li>
+            </NavMenu>
+          </UlList>
+        </nav>
+      <div className="App">
+        <p>Hello world</p>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+            <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    
+  );
+}
+const About = () => <p>About</p>
+const Users = () => <p>Users</p>
+const Home = () => <p>Home</p>
+
+export default App;
