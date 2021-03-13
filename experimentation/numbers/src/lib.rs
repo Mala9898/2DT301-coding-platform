a9488d88dde055876a787b6b7d4adc6ba5792964
Stan <6561374+Mala9898@users.noreply.github.com>
2021-03-13 19:30:31 +0100

    ch 2 done

--- experimentation/numbers/src/lib.rs
+++ experimentation/numbers/src/lib.rs
@@ -1,20 +1,27 @@
 pub fn say_hello() {
     println!("hello world!!!! -- from lib.rs");
 }
-pub fn print() -> () {
-    let numbers = [1,2,3,4,5];
-    for n in numbers.iter() {
+pub fn print(num: u8) -> () {
+    let numbers: Vec<u8> = generate_sequence(num);
+    output_sequence(&numbers);
+    
+}
+fn generate_sequence(limit: u8) -> Vec<u8> {
+    // let mut numbers = Vec::new();
+    // for n in 1..=limit  {
+    //     numbers.push(n);
+    // }
+    // numbers
+    (1..=limit).collect()
+}
+fn output_sequence(numbers: &[u8]) {
+    for n in numbers {
         println!("{}", n);
     }
-    output_sequence(numbers);
-    // let numbers = vec![1,2,3,4,5];
-    // for n in numbers {
-    //     println!("{}", n);
-    // }
 }
 
-fn output_sequence(numbers: [u8; 5]) {
-    for n in numbers.iter() {
-        println!("{}", n);
-    }
+#[test]
+fn generate_sequence_should_work() {
+    let result = generate_sequence(3);
+    assert_eq!(result, &[1,2,3]);
 }
--- experimentation/numbers/src/main.rs
+++ experimentation/numbers/src/main.rs
@@ -1,11 +1,12 @@
 use numbers::{ say_hello };
+// use std::vec::Vec;
 
 fn main() {
     // println!("Hello, world!");
     // nothing();
     say_hello();
 
-    numbers::print();
+    numbers::print(10);
 
 }
 
